55f156848ee4082790c7fc03bff94d6c1bdcdebb
Angus Macdonald <angus.d.macdonald@gmail.com>
2016-07-09 17:28:57 -0400

    extract main controller to file

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -70,33 +70,3 @@ function resolveReceiversRequest($stateParams, receivers) {
 
 
 
-
-app.controller('MainCtrl', [
-  '$scope',
-  '$state',
-  'receivers',
-  'teams',
-  'years',
-  function($scope, $state, receivers, teams, years) {
-    $scope.teams = teams.teams;
-    $scope.years = years.years;
-
-    $scope.display = receivers.display;
-
-    $scope.updateDisplay = function(chart) {
-      // Change the URL, initiating a new request.
-      var nextState = 'home' + (($scope.display.length > 1) ? "." + $scope.display.length: "");
-
-      // Create query parameters from all defined team-year pairs:
-      var params = {};
-
-      for (i = 0; i < $scope.display.length; i++) {
-        var letter = String.fromCharCode(65 + i);
-        params['team' + letter] = $scope.display[i].selectedTeam;
-        params['year' + letter] = $scope.display[i].selectedYear;
-      }
-
-      $state.go(nextState, params);
-    };
-  }
-]);
